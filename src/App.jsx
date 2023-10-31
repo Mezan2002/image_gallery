cdc660553b644892d06cbe0a98997e7670bb27c7
Mezan2002 <mezanurr009@gmail.com>
2023-10-31 23:28:40 +0600

    Initialitions done and tailwind css installed as a CSS framework

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1 className="text-5xl text-center">Hello World</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
